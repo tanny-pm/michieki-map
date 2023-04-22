66302602fbc628dd7b88ea4765cc6c046cb84be2
Tanny <ryosuke.tanimura+dev@gmail.com>
2023-04-22 17:04:42 +0900

    Fix: delete stationName

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -149,7 +149,6 @@ const Map: React.FC<MapProps> = ({ zoom, center }) => {
         map.on("click", "station-points", (e) => {
           if (e.features && e.features.length > 0) {
             const feature = e.features[0];
-            const stationName = feature.properties?.P35_006;
             popup
               .setLngLat(feature.geometry.coordinates)
               .setHTML(
